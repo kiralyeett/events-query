d114da0d74df10131a9063f3ef01c85a919cc119
kiralyeett <kiralyet@yahoo.com>
2016-05-19 10:51:57 +0200

    Update Events.sql
    where clause changed, added 'inside queried interval' the end clock

--- Events.sql
+++ Events.sql
@@ -2,6 +2,6 @@ select * from `events`
 where 
  (startd<='2016-05-01' and endd>='2016-05-01') -- begins before queried interval, and ends inside or outside interval
  OR
- (startd>='2016-05-01' and endd<='2016-05-31') -- inside queried interval
+ (startd>='2016-05-01' and endd<='2016-05-31 23:59:59') -- inside queried interval
  OR
  (startd<='2016-05-31' and endd>='2016-05-31') -- begins inside interval but and outside interval
